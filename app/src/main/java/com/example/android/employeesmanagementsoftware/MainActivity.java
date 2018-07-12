6d3f8d03233cf7a2e7de0a8298cd26a0eea24f85
Monica <monica_charles@ymail.com>
2018-07-12 15:33:41 +0200

    Employee&DepartmentLayouts

--- app/src/main/java/com/example/android/employeesmanagementsoftware/MainActivity.java
+++ app/src/main/java/com/example/android/employeesmanagementsoftware/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.employeesmanagementsoftware;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button emp = (Button)findViewById(R.id.emp);
+        emp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent employeeActivity = new Intent(MainActivity.this,EmployeeActivity.class);
+                startActivity(employeeActivity);
+            }
+        });
+
+        Button dep = (Button)findViewById(R.id.dep);
+        dep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent departementActivity = new Intent(MainActivity.this,DepartmentActivity.class);
+                startActivity(departementActivity);
+            }
+        });
     }
 }
