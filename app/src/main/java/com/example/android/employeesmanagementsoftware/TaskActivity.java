3e4b5ad257076feb7e3118a30c821cb941b659a1
raffy-bekhit <raffybekhit@gmail.com>
2018-07-17 19:57:45 +0200

    minor fixes

--- app/src/main/java/com/example/android/employeesmanagementsoftware/TaskActivity.java
+++ app/src/main/java/com/example/android/employeesmanagementsoftware/TaskActivity.java
@@ -1,13 +1,21 @@
 package com.example.android.employeesmanagementsoftware;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.android.employeesmanagementsoftware.data.DBHelpers.EmployeesManagementDbHelper;
+import com.example.android.employeesmanagementsoftware.data.Contracts.TaskContract;
+import com.example.android.employeesmanagementsoftware.data.Contracts.TaskContract.TaskEntry;
+
 
 public class TaskActivity extends AppCompatActivity {
 
+
+    EmployeesManagementDbHelper emdb = new EmployeesManagementDbHelper(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,4 +40,44 @@ public class TaskActivity extends AppCompatActivity {
         deadlinetext.setText("Deadline: "+deadline);
 
 }
+
+public Cursor getTasksCursor(){
+        SQLiteDatabase db  = emdb.getReadableDatabase(); //get readable instance of the db
+
+    //specify the columns to be read
+        String [] columns = {
+                TaskEntry.COLUMN_TASK_NAME,
+                TaskEntry.COLUMN_TASK_DESCRIPTION,
+                TaskEntry.COLUMN_TASK_EVALUATION,
+                TaskEntry.COLUMN_TASK_DEADLINE
+        };
+
+    //cursor is a table containing the rows returned form the query
+        Cursor cursor =  db.query(TaskContract.TABLE_NAME,columns,null,null,null,null,null);
+
+        return cursor;
+
+    }
+
+    public Cursor getTasksCursor(int task_id){
+
+        SQLiteDatabase db  = emdb.getReadableDatabase(); //get readable instance of the db
+
+        //specify the columns to be read
+        String [] columns = {
+                TaskEntry.COLUMN_TASK_NAME,
+                TaskEntry.COLUMN_TASK_DESCRIPTION,
+                TaskEntry.COLUMN_TASK_EVALUATION,
+                TaskEntry.COLUMN_TASK_DEADLINE
+        };
+
+        String selection
+
+        //cursor is a table containing the rows returned form the query
+        Cursor cursor =  db.query(TaskContract.TABLE_NAME,columns,null,null,null,null,null);
+
+        return cursor;
+
+    }
+
 }
