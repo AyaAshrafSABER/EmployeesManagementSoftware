162c5335cbcb28a79a06f65192c66b9b8da782a3
Aya <ayasaberosman@gmail.com>
2018-08-08 01:23:30 +0200

    make the starting view with empty frigmant we should make all activity be a frigmant

--- app/src/main/java/com/example/android/employeesmanagementsoftware/MainActivity.java
+++ app/src/main/java/com/example/android/employeesmanagementsoftware/MainActivity.java
@@ -2,10 +2,18 @@ package com.example.android.employeesmanagementsoftware;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
 /*
 made by Aya
  */
@@ -16,27 +24,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//         TasksFragment fragment = new TasksFragment();
-//         getSupportFragmentManager()
-//                 .beginTransaction()
-//                 .replace(R.id.frame, fragment)
-//                 .commit();
-        Button emp = (Button)findViewById(R.id.emp);
-        emp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent employeeActivity = new Intent(MainActivity.this,EmployeeActivity.class);
-                startActivity(employeeActivity);
-            }
-        });
+    }
 
-        Button dep = (Button)findViewById(R.id.dep);
-        dep.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent departementActivity = new Intent(MainActivity.this,DepartmentActivity.class);
-                startActivity(departementActivity);
-            }
-        });
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.new_game:
+                Toast.makeText(this, "first", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.help:
+                Toast.makeText(this, "second", Toast.LENGTH_LONG).show();
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
