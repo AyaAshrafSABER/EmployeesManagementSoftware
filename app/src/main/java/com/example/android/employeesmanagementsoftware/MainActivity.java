9af0ff66048d71c495d4ecbd2b9dff537d53d7e3
Aya <ayasaberosman@gmail.com>
2018-08-10 03:25:37 +0200

    make a main page

--- app/src/main/java/com/example/android/employeesmanagementsoftware/MainActivity.java
+++ app/src/main/java/com/example/android/employeesmanagementsoftware/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.employeesmanagementsoftware;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,15 +16,19 @@ made by Aya
  */
 // see how to connect 3 main activity ** department ** employies **tasks
 public class MainActivity extends AppCompatActivity {
-
+    private static int SPLASH_TIME_OUT = 750;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//         TasksFragment fragment = new TasksFragment();
-//         getSupportFragmentManager()
-//                 .beginTransaction()
-//                 .replace(R.id.frame, fragment)
-//                 .commit();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home = new Intent(MainActivity.this,StartingPageActivity.class);
+                startActivity(home);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
     }
 }
