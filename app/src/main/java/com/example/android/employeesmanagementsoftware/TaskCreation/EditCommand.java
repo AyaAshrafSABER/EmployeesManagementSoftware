3d223b428a5b176db545b5741a87204d6c9bb90e
omar_swidan <oswidan97@gmail.com>
2018-08-15 22:21:19 +0200

    edit texts are set in the edit mode according to the task being edited

--- app/src/main/java/com/example/android/employeesmanagementsoftware/TaskCreation/EditCommand.java
+++ app/src/main/java/com/example/android/employeesmanagementsoftware/TaskCreation/EditCommand.java
@@ -3,11 +3,13 @@ package com.example.android.employeesmanagementsoftware.TaskCreation;
 import android.app.Activity;
 import android.content.Context;
 import android.database.Cursor;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 import com.example.android.employeesmanagementsoftware.R;
 import com.example.android.employeesmanagementsoftware.data.Contracts.EmployeeContract;
+import com.example.android.employeesmanagementsoftware.data.Contracts.TaskContract;
 import com.example.android.employeesmanagementsoftware.data.DBHelpers.EmployeesManagementDbHelper;
 
 import java.util.Set;
@@ -18,7 +20,7 @@ public class EditCommand implements TaskCreationCommand {
     private Activity activity;
     private  EmployeesManagementDbHelper employeeDBHelper;
     private long task_id;
-
+    private final String TAG="edit";
     EditCommand(Activity activity, EmployeesManagementDbHelper employeeDBHelper, long task_id) {
     this.activity=activity;
     this.employeeDBHelper=employeeDBHelper;
@@ -31,7 +33,15 @@ public class EditCommand implements TaskCreationCommand {
         EditText taskName=activity.findViewById(R.id.task_name_edit);
         EditText taskDescription=activity.findViewById(R.id.department_description_edit_text);
         EditText taskDeadline=activity.findViewById(R.id.task_deadline_edit);
+
         Cursor textCursor=employeeDBHelper.getSpecifiTaskCursor(task_id);
+        textCursor.moveToNext();
+
+        taskName.setText(textCursor.getString(textCursor.getColumnIndex(TaskContract.TaskEntry.COLUMN_TASK_NAME)));
+        taskDeadline.setText(textCursor.getString(textCursor.getColumnIndex(TaskContract.TaskEntry.COLUMN_TASK_DEADLINE)));
+        taskDescription.setText(textCursor.getString(textCursor.getColumnIndex(TaskContract.TaskEntry.COLUMN_TASK_DESCRIPTION)));
+
+        textCursor.close();
 
         Cursor cursor=employeeDBHelper.getEmployeesOfTask(task_id);
         cursor.moveToNext();
