5671f0c6f8f9dcadee55bfd4cf4823fec579bbe0
Monica <moca.charles@gmail.com>
2018-08-16 16:08:04 +0200

    EmployeeClass

--- app/src/main/java/com/example/android/employeesmanagementsoftware/EmployeeDB/Employee.java
+++ app/src/main/java/com/example/android/employeesmanagementsoftware/EmployeeDB/Employee.java
@@ -1,5 +1,12 @@
 package com.example.android.employeesmanagementsoftware.EmployeeDB;
 
+import android.support.annotation.NonNull;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Created by User on 8/16/2018.
  */
@@ -9,6 +16,13 @@ public class Employee {
    private String name,phone,mail,birthdate,job,image;
    private boolean isImgChanged;
    private long id;
+
+   private Map<Long,String> notes;
+
+   public Employee(){
+       notes = new HashMap<Long, String>() ;
+
+   }
 
 
 }
